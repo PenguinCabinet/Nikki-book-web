65e528d59b947cb8cd28074dcaea2aa328f8d51b
PenguinCabinet <kage2.kage22@gmail.com>
2026-04-02 22:44:01 +0900

    デバッグ用printを削除

--- backend/main.py
+++ backend/main.py
@@ -17,17 +17,6 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-# secret
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$argon2id$v=19$m=65536,t=3,p=4$wagCPXjifgvUFBzq4hqe3w$CYaIb8sB+wtD+Vu/P4uod1+Qof8h+1g7bbDlBID48Rc",
-        "disabled": False,
-    }
-}
-
 
 class Token(BaseModel):
     access_token: str
@@ -90,7 +79,6 @@ def get_password_hash(password):
 
 def get_user(session: SessionDep, username: str):
     user = session.exec(select(User).where(User.username == username)).all()
-    print(user)
     if len(user)==1:
         return user[0]
 
@@ -139,8 +127,6 @@ async def get_current_user(session: SessionDep,token: Annotated[str, Depends(oau
 async def get_current_active_user(
     current_user: Annotated[User, Depends(get_current_user)],
 ):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
 
