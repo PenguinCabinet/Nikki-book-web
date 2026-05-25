a339a68d8143889d9f61110ae64e9b8c6a2f06b5
PenguinCabinet <kage2.kage22@gmail.com>
2026-05-26 00:27:23 +0900

    CSRF対策にsamesite="lax"にする。オリジンを統一した

--- backend/main.py
+++ backend/main.py
@@ -174,7 +174,7 @@ async def login_for_access_token(
         value=session_id,
         httponly=True,
         max_age=2147483647,
-        samesite="none",
+        samesite="lax",
         secure=(os.getenv("ENV") == "production"),
     )
     return {"status": "success"}
